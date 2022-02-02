34a8b8aeec83fcd411016d7e6394d48c3888dc51
pearlymo <mothi.ghimire@gmail.com>
2022-02-02 22:09:35 +0000

    create a new user and ask for username and password

--- install_chroot.sh
+++ install_chroot.sh
@@ -29,3 +29,49 @@ timedatectl set-timezone "US/Mountain"
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+# No argument: ask for a username.
+# One argument: use the username passed as argument.
+function config_user() {
+    if [ -z "$1" ]; then
+        dialog --no-cancel --inputbox "Please enter your username." \
+            10 60 2> name
+    else
+        echo "$1" > name
+    fi
+    dialog --no-cancel --passwordbox "Enter your password." \
+        10 60 2> pass1
+    dialog --no-cancel --passwordbox "Confirm your password." \
+        10 60 2> pass2
+    while [ "$(cat pass1)" != "$(cat pass2)" ]
+    do
+        dialog --no-cancel --passwordbox \
+            "Passwords do not match.\n\nEnter password again." \
+            10 60 2> pass1
+        dialog --no-cancel --passwordbox \
+            "Retype your password." \
+            10 60 2> pass2
+    done
+
+    name=$(cat name) && rm name
+    pass1=$(cat pass1) && rm pass1 pass2
+
+    # Create user if doesn't exist
+    if [[ ! "$(id -u "$name" 2> /dev/null)" ]]; then
+        dialog --infobox "Adding user $name..." 4 50
+        useradd -m -g wheel -s /bin/bash "$name"
+    fi
+
+    # Add password to user
+    echo "$name:$pass1" | chpasswd
+}
+
+dialog --title "root password" \
+    --msgbox "It's time to add a password for the root user" \
+    10 60
+config_user root
+
+dialog --title "Add User" \
+    --msgbox "Let's create another user." \
+    10 60
+config_user
