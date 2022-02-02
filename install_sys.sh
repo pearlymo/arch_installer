62e929d6b08afd57662dbdf0eace55b6750e2431
pearlymo <mothi.ghimire@gmail.com>
2022-02-02 21:29:00 +0000

    create dialog to ask which hard disk to use for installation

--- install_sys.sh
+++ install_sys.sh
@@ -18,3 +18,17 @@ dialog --no-cancel --inputbox "Enter a name for your computer." \
 # Verify boot (UEFI or BIOS)
 uefi=0
 ls /sys/firmware/efi/efivars 2> /dev/null && uefi=1
+
+# Choosing the hard drive
+devices_list=($(lsblk -d | awk '{print "/dev/" $1 " " $4 " on"}' \
+    | grep -E 'sd|hd|vd|nvme|mmcblk'))
+
+dialog --title "Choose your hard drive" --no-cancel --radiolist \
+"Where do you want to install your new system?\n\n\
+Select with SPACE, valid with ENTER.\n\n\
+WARNING: Everything will be DESTROYED on the hard disk!" \
+15 60 4 "${devices_list[@]}" 2> hd
+
+hd=$(cat hd) && rm hd
+
+
