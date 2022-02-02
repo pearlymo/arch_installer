fb1bf452229fe7de632c795e0ffc20739b7d0bd2
pearlymo <mothi.ghimire@gmail.com>
2022-02-02 22:01:20 +0000

    install grub and create config file for grub

--- install_chroot.sh
+++ install_chroot.sh
@@ -3,3 +3,18 @@
 uefi=$(cat /var_uefi); hd=$(cat /var_hd);
 
 cat /comp > /etc/hostname && rm /comp
+
+pacman --noconfirm -S dialog
+
+pacman -S --noconfirm grub
+
+if [ "$uefi" = 1 ]; then
+    pacman -S --noconfirm efibootmgr
+    grub-install --target=x86_64-efi \
+        --bootloader-id=GRUB \
+        --efi-directory=/boot/efi
+else
+    grub-install "$hd"
+fi
+
+grub-mkconfig -o /boot/grub/grub.cfg
