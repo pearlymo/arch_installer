675a0e09c13be27f075821906757c3156dba4c67
pearlymo <mothi.ghimire@gmail.com>
2022-02-02 22:20:09 +0000

    dialog to choose groups of applications

--- install_apps.sh
+++ install_apps.sh
@@ -5,3 +5,30 @@ name=$(cat /tmp/user_name)
 apps_path="/tmp/apps.csv"
 
 curl https://raw.githubusercontent.com/mothighimire/arch_installer/master/apps.csv > $apps_path
+
+dialog --title "Welcome!" \
+--msgbox "Welcome to the install script for your apps and dotfiles!" \
+    10 60
+
+# Allow the user to select the group of packages he (or she) wants to install.
+apps=("essential" "Essentials" on
+      "network" "Network" on
+      "tools" "Nice tools to have (highly recommended)" on
+      "tmux" "Tmux" on
+      "notifier" "Notification tools" on
+      "git" "Git & git tools" on
+      "i3" "i3 wm" on
+      "zsh" "The Z-Shell (zsh)" on
+      "neovim" "Neovim" on
+      "urxvt" "URxvt" on
+      "firefox" "Firefox (browser)" off
+      "js" "JavaScript tooling" off
+      "qutebrowser" "Qutebrowser (browser)" off
+      "lynx" "Lynx (browser)" off)
+
+dialog --checklist \
+"You can now choose what group of application you want to install. \n\n\
+You can select an option with SPACE and valid your choices with ENTER." \
+0 0 0 \
+"${apps[@]}" 2> app_choices
+choices=$(cat app_choices) && rm app_choices
